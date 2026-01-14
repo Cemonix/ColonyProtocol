2209cc7cbe66c9072bdf77b60b27d08e91875a7f
Cemonix <gittlermilan@seznam.cz>
2026-01-14 18:34:37 +0100

    feat: enhance GameState with player and planet management methods

--- colony_protocol/src/game_state.rs
+++ colony_protocol/src/game_state.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 
 use thiserror::Error;
 
@@ -8,6 +9,12 @@ use super::player::{ PlayerId, Player };
 
 #[derive(Debug, Error)]
 pub enum GameStateError {
+    #[error("Player {0} is already present")]
+    PlayerAlreadyExists(String),
+
+    #[error("Planet {0} is already present")]
+    PlanetAlreadyExists(String),
+
     #[error(transparent)]
     StructureConfigError(#[from] StructureConfigError)
 }
@@ -20,14 +27,38 @@ pub struct GameState {
 }
 
 impl GameState {
-    pub fn new() -> Result<Self, GameStateError> {
+    pub fn new(players: HashMap<PlayerId, Player>, planets: HashMap<PlanetId, Planet>) -> Result<Self, GameStateError> {
         Ok(
             GameState {
-                players: HashMap::new(),
-                planets: HashMap::new(),
+                players: players,
+                planets: planets,
                 turn: 0,
                 structure_config: StructureConfig::load()?
             }
         )
+    }
+
+    pub fn add_player(&mut self, player: Player) -> Result<(), GameStateError> {
+        match self.players.entry(player.id.clone()) {
+            Entry::Vacant(e) => {
+                e.insert(player);
+                Ok(())
+            }
+            Entry::Occupied(_) => {
+                Err(GameStateError::PlayerAlreadyExists(player.name))
+            }
+        }
+    }
+
+    pub fn add_planet(&mut self, planet: Planet) -> Result<(), GameStateError> {
+        match self.planets.entry(planet.id.clone()) {
+            Entry::Vacant(e) => {
+                e.insert(planet);
+                Ok(())
+            }
+            Entry::Occupied(_) => {
+                Err(GameStateError::PlanetAlreadyExists(planet.name))
+            }
+        }
     }
 }
