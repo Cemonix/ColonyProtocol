c93ce53730b75be2bf701e819cec9ca728a8262a
Cemonix <gittlermilan@seznam.cz>
2026-01-20 22:18:40 +0100

    feat: register pending_action module
    - Add pending_action module to main

--- colony_protocol/src/main.rs
+++ colony_protocol/src/main.rs
@@ -2,25 +2,33 @@ mod resources;
 mod configs;
 mod structure;
 mod planet;
+mod planet_name_generator;
 mod player;
+mod pending_action;
+mod map;
 mod game_state;
 mod game_configuration;
 mod game;
 mod commands;
-mod planet_graph;
 mod utils;
 
 use crate::{game::Game, game_configuration::GameConfiguration};
 
 fn main() {
-    let game_configuration = match GameConfiguration::new() {
-            Ok(config) => config,
-            Err(error) => {
-                eprintln!("CRITICAL ERROR: Colonial Command initialization failed - {}", error);
-                eprintln!("Connection terminated. Please restart the protocol.");
-                return;
-            }
-        };
+    #[cfg(debug_assertions)]
+    let config_result = GameConfiguration::debug_default();
+
+    #[cfg(not(debug_assertions))]
+    let config_result = GameConfiguration::new();
+
+    let game_configuration = match config_result {
+        Ok(config) => config,
+        Err(error) => {
+            eprintln!("CRITICAL ERROR: Colonial Command initialization failed - {}", error);
+            eprintln!("Connection terminated. Please restart the protocol.");
+            return;
+        }
+    };
 
     let mut game = match Game::new(game_configuration) {
         Ok(game) => game,
