dee08e649310975b5d46aaf351282b22a9ce2aa1
Cemonix <gittlermilan@seznam.cz>
2026-01-20 22:18:25 +0100

    feat: add cancel command parsing
    - Import CancelArgs
    - Add 'cancel' command parsing in match statement

--- colony_protocol/src/commands/parser.rs
+++ colony_protocol/src/commands/parser.rs
@@ -1,5 +1,7 @@
 use crate::commands::command::{Command, CommandError};
 use crate::commands::build::BuildArgs;
+use crate::commands::cancel::CancelArgs;
+use crate::commands::status::StatusArgs;
 
 pub trait Parseable {
     fn parse(args: Vec<&str>) -> Result<Self, CommandError> where Self: Sized;
@@ -17,6 +19,9 @@ pub fn parse(input: &str) -> Result<Command, CommandError> {
 
     match command_name {
         "build" => Ok(Command::Build(BuildArgs::parse(command_args)?)),
+        "cancel" => Ok(Command::Cancel(CancelArgs::parse(command_args)?)),
+        "status" => Ok(Command::Status(StatusArgs::parse(command_args)?)),
+        "map" => Ok(Command::Map),
         _ => Err(CommandError::UnknownCommand(command_name.to_string())),
     }
 }
